4c6292f226e8beca17a922ee9dffb588ddd880ed
Diego Sosa <diegososajt@gmail.com>
2018-10-25 21:04:09 -0600

    Added basic auth system

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import flask
 import dash
+import dash_auth
 import dash_core_components as dcc
 import dash_html_components as html
+
+VALID_USERNAME_PASSWORD_PAIRS = [
+    ['lago', 'escondido']
+]
 
 server = flask.Flask(__name__)
 app = dash.Dash(__name__, server=server)
 app.config.suppress_callback_exceptions = True
+auth = dash_auth.BasicAuth(
+    app,
+    VALID_USERNAME_PASSWORD_PAIRS
+)
 
 def df_to_table(df):
     return html.Table(
